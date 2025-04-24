35ac8169825acca0099a2a1ee5f9ace3b6dd457c
Pol Gubau Amores <gubaupol@gmail.com>
2025-04-24 19:04:08 +0200

    Add input ID to pantalla and num7 button, and implement basic JavaScript functionality

---
+++ Clase 4/javascript.js
@@ -0,0 +1,24 @@
+// Imports
+const num7 = document.getElementById("num7")
+const pantalla = document.getElementById("pantalla")
+
+
+
+
+
+// Lógica (funciones)
+
+
+function ponValorEnPantalla(num){
+  pantalla.value = num
+}
+
+
+function main(){
+  ponValorEnPantalla(2213213)
+}
+
+
+// Renders 
+
+main()
